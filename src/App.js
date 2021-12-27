5084f08ccdcd7b08bf4a675bb60824463bc9ee56
Arif Hossain <arif04cuet@gmail.com>
2021-12-27 19:05:05 +0600

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import TotalStock from './TotalStock';
 
 function App() {
+
+  const [stocks, setStocks] = useState([])
+
+  const getMeta = (stock, meta) => {
+    return Boolean(stock[meta]?.length) ? stock[meta][0].meta_value : 0
+  }
+
+  useEffect(() => {
+    const url = 'https://api.stocknow.com.bd/v1/fundamentals';
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+
+        const items = [];
+
+        Object.keys(res).forEach(code => {
+
+          const stock = res[code];
+
+          const item = {
+            code: code,
+            paid_up_capital: getMeta(stock, 'paid_up_capital'),
+            earning_per_share: getMeta(stock, 'earning_per_share'),
+            net_asset_val_per_share: getMeta(stock, 'net_asset_val_per_share'),
+            reserve_and_surp: getMeta(stock, 'reserve_and_surp'),
+            total_securities: getMeta(stock, 'total_no_securities')
+              + '|' + getMeta(stock, 'share_percentage_director')
+              + '|' + getMeta(stock, 'share_percentage_foreign')
+              + '|' + getMeta(stock, 'share_percentage_govt')
+              + '|' + getMeta(stock, 'share_percentage_institute')
+              + '|' + getMeta(stock, 'share_percentage_public'),
+          }
+
+          items.push(item);
+
+        });
+
+        setStocks(items)
+
+      })
+      .catch(e => console.log(e.response))
+  }, [])
+
+  const columns = [
+    {
+      dataField: 'code',
+      text: 'Code',
+      sort: true
+    },
+    {
+      dataField: 'paid_up_capital',
+      text: 'PUC',
+      sort: true
+    },
+    {
+      dataField: 'earning_per_share',
+      text: 'EPS',
+      sort: true
+    },
+    {
+      dataField: 'net_asset_val_per_share',
+      text: 'NAV',
+      sort: true
+    },
+    {
+      dataField: 'reserve_and_surp',
+      text: 'Reserve',
+      sort: true
+    },
+    {
+      dataField: 'total_securities',
+      text: 'TS = D-F-G-I-P',
+      sort: true,
+      formatter: (row, cell) => <TotalStock row={row} cell={cell} />
+    }
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <BootstrapTable keyField='code' data={stocks} columns={columns} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/TotalStock.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const TotalStock = ({ row, cell }) => {
+
+    console.log(cell);
+    if (!Boolean(cell))
+        return;
+
+    const total = cell.split('|')
+    return (
+        <>
+            <div className="row">
+                <div className="col">Total</div>
+                <div className="col">{total[0]}</div>
+            </div>
+            <div className="row">
+                <div className="col">F</div>
+                <div className="col">{total[1]}</div>
+            </div>
+            <div className="row">
+                <div className="col">G</div>
+                <div className="col">{total[2]}</div>
+            </div>
+            <div className="row">
+                <div className="col">I</div>
+                <div className="col">{total[3]}</div>
+            </div>
+
+            <div className="row">
+                <div className="col">P</div>
+                <div className="col">{total[4]}</div>
+            </div>
+        </>
+    )
+}
+
+export default TotalStock
