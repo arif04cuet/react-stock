be22096b9c067f2c9a84314dbb7585807d8f2c34
Arif Hossain <arif04cuet@gmail.com>
2021-12-28 21:14:02 +0600

    category added in list

--- src/App.js
+++ src/App.js
@@ -5,55 +5,87 @@ import { useEffect, useState } from 'react';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import TotalStock from './TotalStock';
+import ExpandedRow from './ExpandedRow';
 
 function App() {
 
+  const [instruments, setInstruments] = useState([])
+  const [fundamentals, setFundamentals] = useState([])
   const [stocks, setStocks] = useState([])
+  const [sectors, setSectors] = useState([])
 
   const getMeta = (stock, meta) => {
-    return Boolean(stock[meta]?.length) ? stock[meta][0].meta_value : 0
+    return stock && meta in stock ? stock[meta][0]?.meta_value : 0
+  }
+
+  const formatItems = () => {
+
+    const items = [];
+
+
+    Object.keys(instruments).forEach(code => {
+
+      const fundamental = fundamentals[code];
+      const instrument = instruments[code];
+      console.log(fundamental);
+      const item = {
+        code: code,
+        category: instrument.category,
+        paid_up_capital: getMeta(fundamental, 'paid_up_capital'),
+        earning_per_share: getMeta(fundamental, 'earning_per_share'),
+        net_asset_val_per_share: getMeta(fundamental, 'net_asset_val_per_share'),
+        reserve_and_surp: getMeta(fundamental, 'reserve_and_surp'),
+        total_securities: getMeta(fundamental, 'total_no_securities')
+      }
+
+      items.push(item);
+
+    });
+
+    setStocks(items)
+
+  }
+
+  const fectStockData = async () => {
+
+    const apiBase = 'https://api.stocknow.com.bd/v1';
+    const fundamentalApi = apiBase + '/fundamentals';
+    const instrumentApi = apiBase + '/instruments';
+    const sectorApi = apiBase + '/sectors';
+
+    let response;
+
+    response = await fetch(instrumentApi);
+    const instrumentsData = await response.json();
+    setInstruments(instrumentsData);
+
+    response = await fetch(fundamentalApi);
+    const fundamentalsData = await response.json();
+    setFundamentals(fundamentalsData);
+
+    response = await fetch(sectorApi);
+    const sectorsData = await response.json();
+    setSectors(sectorsData);
+
+
+    formatItems();
   }
 
   useEffect(() => {
-    const url = 'https://api.stocknow.com.bd/v1/fundamentals';
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
 
-        const items = [];
+    fectStockData()
 
-        Object.keys(res).forEach(code => {
-
-          const stock = res[code];
-
-          const item = {
-            code: code,
-            paid_up_capital: getMeta(stock, 'paid_up_capital'),
-            earning_per_share: getMeta(stock, 'earning_per_share'),
-            net_asset_val_per_share: getMeta(stock, 'net_asset_val_per_share'),
-            reserve_and_surp: getMeta(stock, 'reserve_and_surp'),
-            total_securities: getMeta(stock, 'total_no_securities')
-              + '|' + getMeta(stock, 'share_percentage_director')
-              + '|' + getMeta(stock, 'share_percentage_foreign')
-              + '|' + getMeta(stock, 'share_percentage_govt')
-              + '|' + getMeta(stock, 'share_percentage_institute')
-              + '|' + getMeta(stock, 'share_percentage_public'),
-          }
-
-          items.push(item);
-
-        });
-
-        setStocks(items)
-
-      })
-      .catch(e => console.log(e.response))
   }, [])
 
   const columns = [
     {
       dataField: 'code',
       text: 'Code',
+      sort: true
+    },
+    {
+      dataField: 'category',
+      text: 'Category',
       sort: true
     },
     {
@@ -78,18 +110,30 @@ function App() {
     },
     {
       dataField: 'total_securities',
-      text: 'TS = D-F-G-I-P',
-      sort: true,
-      formatter: (cell, row) => <TotalStock row={row} cell={cell} />
+      text: 'TS',
+      sort: true
     }
   ];
+
+  const expandRow = {
+    renderer: row => <ExpandedRow row={row} />,
+    showExpandColumn: true
+  };
 
 
   return (
     <div className="container">
       <div className="row">
         <div className="col">
-          <BootstrapTable keyField='code' data={stocks} columns={columns} />
+          <BootstrapTable
+            keyField='code'
+            data={stocks}
+            columns={columns}
+            striped
+            hover
+            condensed
+            expandRow={expandRow}
+          />
         </div>
       </div>
     </div>
---
+++ src/ExpandedRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpandedRow = ({ row }) => {
+    return (
+        <div>
+            ok
+        </div>
+    )
+}
+
+export default ExpandedRow
